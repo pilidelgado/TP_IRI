1c8d8590e8d41593c3e941df21283454047215d6
Pilar Delgado <piludelgado04@gmail.com>
2023-10-27 12:55:32 -0300

    Versión 3 - 27/10

--- Proyecto/libreria/archivos.cpp
+++ Proyecto/libreria/archivos.cpp
@@ -12,3 +12,28 @@ void hayEspacio(Asistencia[posCliente])
 {
 
 }
+void nuevoSize(NSize &error, cliente*& lista, NMAX, u_int nuevoTam) { //va puntero?
+    if(nuevoTam==0)
+    {
+        error= NSize :: ErrSize;
+        return;
+    }
+    cliente* aux = new cliente[nuevoTam];
+
+    u_int longitud = (NMAX < nuevoTam) ? NMAX: nuevoTam;
+
+    if(aux == nullptr)
+    {
+        error= NTamaño :: ErrNull;
+            return;
+    }
+
+    for(u_int i = 0; i < longitud; i++)
+        aux[i] = lista[i];
+
+
+    delete[] lista;
+    lista = aux;
+
+}
+
